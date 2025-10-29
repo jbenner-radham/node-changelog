8cb0905ad39a2b9ff1242246352b9fc07428641c
James Benner <hello@jamesbenner.com>
2025-10-29 18:06:28 -0400

    Fix flatmap types

--- src/uninst-util-flatmap.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Node, Nodes } from 'mdast';
-
-declare module 'unist-util-flatmap' {
-  export default function flatMap(
-    tree: Nodes,
-    callback: (node: Node, index: number, parent: Node | null) => Node
-  ): Nodes;
-}
---
+++ src/unist-util-flatmap.d.ts
@@ -0,0 +1,8 @@
+declare module 'unist-util-flatmap' {
+  import type { Node, Nodes } from 'unist';
+
+  export default function flatMap(
+    tree: Nodes,
+    callback: (node: Node, index: number, parent: Node | null) => Nodes
+  ): Nodes;
+}
