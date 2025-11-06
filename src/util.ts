cec0ae1a5d6583094cb68f6ade3d847367454cfc
James Benner <hello@jamesbenner.com>
2025-11-05 21:16:19 -0500

    Add commented out date code

--- src/util.ts
+++ src/util.ts
@@ -7,6 +7,13 @@ export function getDate(): string {
   return new Date().toISOString()
     .split('T')
     .at(0)!;
+
+  // const date = new Date();
+  // const year = date.getFullYear();
+  // const month = String(date.getMonth() + 1).padStart(2, '0');
+  // const day = String(date.getDate()).padStart(2, '0');
+  //
+  // return `${year}-${month}-${day}`;
 }
 
 export function getRepositoryTaggedReleaseUrl(repository: string, version: string): string {
