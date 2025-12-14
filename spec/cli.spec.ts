0e55b4abf6c446e588150937198523ba496bc34f
James Benner <hello@jamesbenner.com>
2025-12-14 14:16:09 -0500

    Add initial CLI tests

---
+++ spec/cli.spec.ts
@@ -0,0 +1,18 @@
+import { render } from 'cli-testing-library';
+import { describe, expect, it } from 'vitest';
+
+describe('cli', () => {
+  const description = 'A tool for managing Keep a Changelog (https://keepachangelog.com/) changelogs.';
+
+  it('displays help text when ran with no flags or commands', async () => {
+    const { findByText } = await render('changelog');
+
+    expect(await findByText(description)).toBeInTheConsole();
+  });
+
+  it('displays help text when passed the help flag', async () => {
+    const { findByText } = await render('changelog', ['--help']);
+
+    expect(await findByText(description)).toBeInTheConsole();
+  });
+});
