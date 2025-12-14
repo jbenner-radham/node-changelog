320410b980c95daaafa605b40a489df9dd6b8b3e
James Benner <hello@jamesbenner.com>
2025-12-14 18:16:22 -0500

    Experiment with render command path

--- spec/cli.spec.ts
+++ spec/cli.spec.ts
@@ -5,12 +5,13 @@ describe('cli', () => {
   const description = 'A tool for managing Keep a Changelog (https://keepachangelog.com/) changelogs.';
 
   it('displays help text when ran with no flags or commands', async () => {
-    const { findByText } = await render('changelog');
+    // const { findByText } = await render('changelog');
+    const { findByText } = await render('./dist/esm/cli.js', [], { cwd: process.cwd() });
 
     expect(await findByText(description)).toBeInTheConsole();
   });
 
-  it('displays help text when passed the help flag', async () => {
+  it.skip('displays help text when passed the help flag', async () => {
     const { findByText } = await render('changelog', ['--help']);
 
     expect(await findByText(description)).toBeInTheConsole();
