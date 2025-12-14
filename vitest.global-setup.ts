b2f6df68bceacdb33439d4d985f50b124f6a0e86
James Benner <hello@jamesbenner.com>
2025-12-14 18:19:39 -0500

    Add console.debug statement

--- vitest.global-setup.ts
+++ vitest.global-setup.ts
@@ -2,6 +2,8 @@ import { $ } from 'execa';
 import type { TestProject } from 'vitest/node';
 
 export async function setup(project: TestProject) {
+  console.debug(process.env.PATH);
+
   await $`pnpm run build`;
 
   project.onTestsRerun(async () => {
