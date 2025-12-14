0e388fba3b1d1c50968bbabfbda9865b5defa25a
James Benner <hello@jamesbenner.com>
2025-12-14 18:02:58 -0500

    Try different pre-test build method

--- vitest.global-setup.ts
+++ vitest.global-setup.ts
@@ -1,8 +1,9 @@
-import { $ } from 'execa';
+import { $, execaSync } from 'execa';
 import type { TestProject } from 'vitest/node';
 
-export async function setup(project: TestProject) {
-  await $`pnpm run build`;
+export function setup(project: TestProject) {
+  // await $`pnpm run build`;
+  execaSync('pnpm', ['run', 'build']);
 
   project.onTestsRerun(async () => {
     await $`pnpm run build`;
