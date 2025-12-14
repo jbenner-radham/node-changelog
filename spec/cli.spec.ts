4e63feeee1daddfc37707d9ef980d1e7dc6a5127
James Benner <hello@jamesbenner.com>
2025-12-14 18:29:41 -0500

    Hopefully fix tests

--- spec/cli.spec.ts
+++ spec/cli.spec.ts
@@ -1,18 +1,22 @@
 import { render } from 'cli-testing-library';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { describe, expect, it } from 'vitest';
 
 describe('cli', () => {
+  const filename = fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+  const changelog = path.resolve(dirname, '..', 'dist', 'esm', 'cli.js');
   const description = 'A tool for managing Keep a Changelog (https://keepachangelog.com/) changelogs.';
 
   it('displays help text when ran with no flags or commands', async () => {
-    // const { findByText } = await render('changelog');
-    const { findByText } = await render('changelog', [], { cwd: process.cwd() });
+    const { findByText } = await render(changelog);
 
     expect(await findByText(description)).toBeInTheConsole();
   });
 
-  it.skip('displays help text when passed the help flag', async () => {
-    const { findByText } = await render('changelog', ['--help']);
+  it('displays help text when passed the help flag', async () => {
+    const { findByText } = await render(changelog, ['--help']);
 
     expect(await findByText(description)).toBeInTheConsole();
   });
--- vitest.global-setup.ts
+++ vitest.global-setup.ts
@@ -2,8 +2,6 @@ import { $ } from 'execa';
 import type { TestProject } from 'vitest/node';
 
 export async function setup(project: TestProject) {
-  console.debug(process.env.PATH);
-
   await $`pnpm run build`;
 
   project.onTestsRerun(async () => {
